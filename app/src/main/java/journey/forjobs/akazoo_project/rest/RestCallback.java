aab60341e5f721452fa7f360ebf07bcf9820584f
ioannisdaratzikis <ioannisdaratzikis@gmail.com>
2017-05-05 18:18:05 +0300

    isError catch

--- app/src/main/java/journey/forjobs/akazoo_project/rest/RestCallback.java
+++ app/src/main/java/journey/forjobs/akazoo_project/rest/RestCallback.java
@@ -4,7 +4,10 @@ import android.util.Log;
 
 import java.util.List;
 
+import journey.forjobs.akazoo_project.R;
+import journey.forjobs.akazoo_project.application.AkazooApplication;
 import journey.forjobs.akazoo_project.model.Playlist;
+import journey.forjobs.akazoo_project.rest.pojos.GenericResponse;
 import journey.forjobs.akazoo_project.utils.Const;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -28,10 +31,11 @@ public abstract class RestCallback<T> implements Callback<T> {
     public void onResponse(Call<T> call, Response<T> response) {
         Log.i(Const.LOG_REST_TAG, "Generic success");
         T responseContent = (T) response.body();
-        if (response.isSuccessful()){
-            handleSuccess(responseContent);
+        GenericResponse castingResponse = (GenericResponse) responseContent;
+        if (castingResponse.isError()){
+            handleFailure(castingResponse.getErrorId(), AkazooApplication.getmInstance().getResources().getString(R.string.generic_rest_error_message));
         }else {
-            handleFailure(response.code(), response.message());
+            handleSuccess(responseContent);
         }
     }
 
